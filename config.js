00a809ed36add897eccf894ed9323b419eb9b5d1
panda082279 <168049710+panda082279@users.noreply.github.com>
2024-06-16 14:04:07 -0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.autoReadAll = process.env.AUTO_READ_ALL || false;
 global.antitags = process.env.ANTITAGS || true;
 
 // Auto functioner
-global.autoTyping = process.env.AUTO_TYPING || true
+global.autoTyping = process.env.AUTO_TYPING || true;
 global.autoRecord = process.env.AUTO_RECORD || false;
 global.groupevent = process.env.GROUPEVENT || false;
 global.statusseen = process.env.STATUSSEEN || true;
@@ -16,7 +16,7 @@ global.autoreadgc = process.env.AUTOREADGC || true;
 
 
 // Auth information
-global.pairNumber = 233532082279                         // Add your paining number with country code example "916297175943"; 
+global.pairNumber = "233532082279";                        // Add your paining number with country code example "916297175943"; 
 global.port = process.env.PORT || "10000";
 global.auth = process.env.AUTH || "Pairing";                // Auth mode OR/Pairing.
 global.sessionFile = process.env.SESSION_FILE || "A17-SESSION";
@@ -30,8 +30,8 @@ global.prefa = process.env.PREFIX ? process.env.PREFIX.split ["."]
 
 
 // Owner information
-global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["233532082279"];
-global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["233532082279"];
+global.Owner = process.env.OWNER ? process.env.OWNER.split["233532082279"];
+global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split["233532082279"];
 global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["916297175943"];
 global.OwnerName = process.env.OWNER_NAME || "Yatti";
 global.BotName = process.env.BOT_NAME || "Yatti";
